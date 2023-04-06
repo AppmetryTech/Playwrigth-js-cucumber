b25f31ac558c141a225a4e72d8a0bf0041abb5b0
MotghareChetan <chetan@appmetry.com>
2023-04-06 12:16:48 +0530

    Update login.page.js

--- page_objects/login.page.js
+++ page_objects/login.page.js
@@ -1,10 +1,12 @@
 const { config } = require("../config/config")
 
 locators = {
-    "username_input": "#user-name",
-    "password_input": "#password",
+    "username_input": "input[data-qa='login-email']",
+    "password_input": "input[type='password']",
     "login_button": "//a[contains(text(),'Signup / Login')]",
-    "inventory_container": "#inventory_container"
+    "clickLogin": "button[data-qa='login-button']",
+    "validateUsername": "//ul[@class='nav navbar-nav']//b[1]",
+    "errorMessage": "//input[@data-qa='login-password']/following-sibling::p[1]"
 }
 
 class LoginPage {
@@ -17,18 +19,32 @@ class LoginPage {
     async clickOnLoginBtn() {
         return await page.click(locators.login_button);
     }
+    async enterEmail(userName) {
+        const element = await page.waitForSelector(locators.username_input);
+        return await page.fill(locators.username_input, userName);
+    }
+    async enterPassword(password) {
+        return await page.fill(locators.password_input, password);
+    }
+    async clickLogin() {
+        return await page.click(locators.clickLogin);
+    }
+
+
 
     async submitLoginForm() {
         const element = await page.waitForSelector(locators.username_input);
-        await page.fill(locators.username_input, 'standard_user');
-        await page.fill(locators.password_input, 'secret_sauce');
+        await page.fill(locators.username_input, '');
+        await page.fill(locators.password_input, 'Test@1234');
         await page.click(locators.login_button);
     }
 
     async verifyAfterLoginPage() {
-        await page.waitForSelector(locators.inventory_container);
-        const visible = await page.isVisible(locators.inventory_container);
-        return expect(visible).to.equal(true);
+        await page.waitForSelector(locators.validateUsername);
+        const actualUsername = await page.textContent(locators.validateUsername);
+        await expect(actualUsername).toContain("Chetan Motghare");
+        /*const visible = await page.isVisible(locators.inventory_container);
+        return expect(visible).to.equal(true);*/
     }
 }
 
